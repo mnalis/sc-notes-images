f760cca9360eebfea295b7a0070a6d2e5e90b5bc
Matija Nalis (Tomsoft) <mnalis-git-torres@voyager.hr>
2021-12-25 00:49:15 +0100

    initial code start from https://github.com/osm-hr/my-osm-notes

---
+++ download_planet_notes.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Matija Nalis <mnalis-git-openstreetmap@voyager.hr> GPLv3+ started 20150201
+# download lastest OSN Notes planet dump
+
+WGET="wget -N -q"
+URL_BASE="http://planet.osm.org/notes"
+PLANET_NOTES_FILE="planet-notes-latest.osn.bz2"
+PLANET_NOTES_MD5="$PLANET_NOTES_FILE.md5"
+
+$WGET "$URL_BASE/$PLANET_NOTES_FILE" || exit 1
+$WGET "$URL_BASE/$PLANET_NOTES_MD5"  || exit 2
+md5sum --status -c $PLANET_NOTES_MD5 || exit 3
+
+echo "$URL_BASE/$PLANET_NOTES_FILE: `ls -l $PLANET_NOTES_FILE`"
+if [ ! -e "OK.$PLANET_NOTES_FILE" -o "$PLANET_NOTES_FILE" -nt "OK.$PLANET_NOTES_FILE" ]
+then
+	cp -a "$PLANET_NOTES_FILE" "OK.$PLANET_NOTES_FILE"
+	exec ./sc_images_parser.pl
+	exit 99
+else
+	echo "No updates."
+	exit 0
+fi
