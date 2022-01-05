8171716433bafa4db2cda4ebfc98215f55b691ab
Matija Nalis (Tomsoft) <mnalis-git-torres@voyager.hr>
2022-01-05 04:11:13 +0100

    do not try to geotag file if it failed to download (eg. 404)

--- sc_images_parser.pl
+++ sc_images_parser.pl
@@ -175,8 +175,13 @@ sub save_pic($) {
       $DEBUG > 1 && say "Note $last_noteid: skipping; $pic_file already downloaded";
    } else {
       $DEBUG > 0 && say "Note $last_noteid: Downloading $url to $pic_file, and adding GPS coordinates";
-      system 'wget', '-q', $url, '-O', $pic_file;	# FIXME: make user configurable for eg. curl?
-      system 'exiftool', '-q', '-P', '-overwrite_original', '-GPSLatitude=' . $last_lat, '-GPSLongitude=' . $last_lon, $pic_file;
+      system 'wget', '-q', '--no-clobber', $url, '-O', $pic_file;	# FIXME: make user configurable for eg. curl?
+      if (-s $pic_file) {
+          system 'exiftool', '-q', '-P', '-overwrite_original', '-GPSLatitude=' . $last_lat, '-GPSLongitude=' . $last_lon, $pic_file;
+      } else {
+          $DEBUG > 1 && say "Note $last_noteid: skipping geotagging; $pic_file does not exists or failed to download";
+          unlink $pic_file;
+      }
    }
    return '';
 }
