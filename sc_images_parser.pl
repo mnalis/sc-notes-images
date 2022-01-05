3667132a3577a9a7002843ff4344ecaa0d0a9f58
Matija Nalis (Tomsoft) <mnalis-git-torres@voyager.hr>
2022-01-05 03:30:05 +0100

    user specified parameters instead of hardcoded values

--- sc_images_parser.pl
+++ sc_images_parser.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -T
+#!/usr/bin/perl
 # initial code from https://github.com/osm-hr/my-osm-notes by Matija Nalis <mnalis-git-openstreetmap@voyager.hr> GPLv3+ 2015-2020
 # 
 # parses OSM notes planet dump and geotag & locally cache StreetComplete images linked
@@ -9,8 +9,8 @@
 
 # FIXME: FIXMEs in code
 # FIXME: add README.md, COPYING
-# FIXME: cmdline arguments for FILTER_BBOX, FILTER_USERS
 # FIXME: cron example (and install cron)
+# FIXME: detect and bail out early if wget/exiftool are not present
 
 use utf8;
 
@@ -22,28 +22,53 @@ use feature 'say';
 use XML::Parser;
 use Encode;
 use Date::Parse qw /str2time/;
-#use Data::Dumper;
+use Getopt::Long;
+use Data::Dumper;
 
-$ENV{'PATH'} = '/usr/local/bin:/usr/bin:/bin';
-
-my $PICTURE_DIR = './images';
-my $MAX_AGE_DAYS = 14;	# ignore pictures in notes older than this many days
-
-#my %FILTER_USERS = map { $_ => 1 } ('Matija Nalis', 'mnalis ALTernative');
-my %FILTER_USERS = ();
-
-my %FILTER_BBOX = ( lon_min => 12.7076, lat_min => 41.6049, lon_max => 19.7065, lat_max => 46.5583 );
-#my %FILTER_BBOX = ();
-
-
-my $OSN_FILE = 'OK.planet-notes-latest.osn.bz2';
-#$OSN_FILE = 'example2.xml.bz2';	# FIXME DELME
+#$ENV{'PATH'} = '/usr/local/bin:/usr/bin:/bin';
 
 #
 # No user serviceable parts below
 #
 
-my $DEBUG = $ENV{DEBUG} || 0;
+my $DEBUG = 1;						# use '-v 0' to disable any output
+my $DECOMPRESSOR = 'bzcat'; $DECOMPRESSOR = 'pbzip2 -dc' if -x '/usr/bin/pbzip2';	# autodetect fast alternative for unpacking .bz2
+my $PICTURE_DIR = './images';				# default directory to store geotagged pictures
+my $MAX_AGE_DAYS = 14;					# ignore pictures in notes older than this many days
+my $OSN_FILE = '';					# default .osn.bz2 file with OSM Notes XML
+my %FILTER_USERS = ();
+
+my $opt_help = '';
+my $opt_filter_bbox = '';
+my @opt_filter_user = ();
+
+my $opts = GetOptions (	"verbose:+"		=>	\$DEBUG,
+                        "picdir=s"		=>	\$PICTURE_DIR,
+                        "maxdays=i"		=>	\$MAX_AGE_DAYS,
+                        "notesfile=s"		=>	\$OSN_FILE,
+                        "decompressor=s"	=>	\$DECOMPRESSOR,
+                        "bbox=s"		=>	\$opt_filter_bbox,
+                        "user=s"		=>	\@opt_filter_user,
+                        "help|h|?"		=>	\$opt_help,
+) or usage();
+
+%FILTER_USERS = map { $_ => 1 } @opt_filter_user if @opt_filter_user;
+
+#my %FILTER_BBOX = ( lon_min => 12.7076, lat_min => 41.6049, lon_max => 19.7065, lat_max => 46.5583 );	# Croatia example
+my %FILTER_BBOX = ();
+if (my @b = split /, */, $opt_filter_bbox) {
+    $FILTER_BBOX{'lon_min'} = shift @b;
+    $FILTER_BBOX{'lat_min'} = shift @b;
+    $FILTER_BBOX{'lon_max'} = shift @b;
+    $FILTER_BBOX{'lat_max'} = shift @b;
+}
+
+die "Directory --picdir=$PICTURE_DIR must exist" unless -d $PICTURE_DIR;
+die "You should use either --bbox or --user to filter results" if !$opt_filter_bbox and !@opt_filter_user;
+
+usage() if $opt_help or !$OSN_FILE;
+
+
 
 my $pic_count = undef;
 my $last_noteid = undef;
@@ -54,10 +79,9 @@ my $last_user = undef;
 my $last_string = undef;
 
 my $start_time = time;
-$DEBUG > 1 && say "parsing $OSN_FILE... ";
+$DEBUG > 1 && say "parsing $DECOMPRESSOR $OSN_FILE... ";
 
-#open my $xml_file, '-|', "bzcat $OSN_FILE";
-open my $xml_file, '-|', "pbzip2 -dc $OSN_FILE";
+open my $xml_file, '-|', "$DECOMPRESSOR $OSN_FILE";
 
 binmode STDOUT, ":utf8"; 
 
@@ -164,4 +188,30 @@ sub end_element
     if ($element eq 'comment') {
         $last_string =~ s{\b(https?://.*?\.jpg)\b}{save_pic($1)}ge if defined $last_string;	# call save_pic($url) for each URL containing .jpg  picture; we don't care about final value of $last_string
     }
+}
+
+# print help
+sub usage {
+        print STDERR <<EOF;
+$0
+  [--help]
+  [--verbose=4]					// use "-v 0" to be quiet unless an error occurs
+  [--picdir=/var/www/pictures]			// save pictures in this directory
+  [--maxdays=14]				// ignore note comments older than this many days
+  [--decompressor=bzcat]			// if not happy with autodetection
+  [--bbox=12.7076,41.6049,19.7065,46.5583]	// limit only to specified BBOX (lon1,lat1,lon2,lat2)
+  [--user='someuser']				// filter by OSM username, can be specified multiply times to match any of them
+  --notesfile=OK.planet-notes-latest.osn.bz2	// specify OSM XML Notes file to parse
+
+Variables:
+  DEBUG:   $DEBUG
+  PICDIR:  $PICTURE_DIR
+  MAXDAYS: $MAX_AGE_DAYS
+  OSNFILE: $OSN_FILE
+  DECOMP:  $DECOMPRESSOR
+EOF
+
+        say '  USERS:   ' . join ', ', keys %FILTER_USERS;
+        say '  BBOX (lon1,lat1,lon2,lat2): ' . (%FILTER_BBOX ? ('(' . $FILTER_BBOX{lon_min} . ', ' . $FILTER_BBOX{lat_min} . ', ' . $FILTER_BBOX{lon_max} . ', ' . $FILTER_BBOX{lat_max} . ')') : 'none');
+        exit (2);
 }
