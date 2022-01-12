67485a9d17073d41939429f2dcae68c8a6935630
Matija Nalis (Tomsoft) <mnalis-git-torres@voyager.hr>
2022-01-12 18:18:28 +0100

    no debug by default

--- download_planet_notes.sh
+++ download_planet_notes.sh
@@ -13,13 +13,13 @@ $WGET "$URL_BASE/$PLANET_NOTES_FILE" || exit 1
 $WGET "$URL_BASE/$PLANET_NOTES_MD5"  || exit 2
 md5sum --status -c $PLANET_NOTES_MD5 || exit 3
 
-echo "$URL_BASE/$PLANET_NOTES_FILE: `ls -l $PLANET_NOTES_FILE`"
+#echo "$URL_BASE/$PLANET_NOTES_FILE: `ls -l $PLANET_NOTES_FILE`"
 if [ ! -e "OK.$PLANET_NOTES_FILE" -o "$PLANET_NOTES_FILE" -nt "OK.$PLANET_NOTES_FILE" ]
 then
 	cp -a "$PLANET_NOTES_FILE" "OK.$PLANET_NOTES_FILE"
 	exec ./sc_images_parser.pl --notesfile "OK.$PLANET_NOTES_FILE" "$@"
 	exit 99
 else
-	echo "No updates."
+	#echo "No updates."
 	exit 0
 fi
